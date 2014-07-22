dee83b880f6ec46cd8922f87300dfcd2a6784086
Kellen Donohue <kellend@cs.washington.edu>
2014-07-21 21:57:43 -0700

    Make light change timer send commands to the default group instead of individual lights

--- JavaDesktopApp/src/com/philips/lighting/gui/LightChangeTimer.java
+++ JavaDesktopApp/src/com/philips/lighting/gui/LightChangeTimer.java
@@ -2,15 +2,11 @@ package com.philips.lighting.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.List;
 
 import javax.swing.DefaultListModel;
 import javax.swing.Timer;
 
-import com.philips.lighting.hue.listener.PHLightListener;
 import com.philips.lighting.hue.sdk.PHHueSDK;
-import com.philips.lighting.model.PHBridge;
-import com.philips.lighting.model.PHLight;
 import com.philips.lighting.model.PHLightState;
 
 public class LightChangeTimer implements ActionListener {
@@ -23,29 +19,18 @@ public class LightChangeTimer implements ActionListener {
     // The the HueSDK singleton.
     final PHHueSDK phHueSDK = PHHueSDK.getInstance();
 
-    // Get the selected bridge.
-    PHBridge bridge = phHueSDK.getSelectedBridge();
-
-    // To get lights use the Resource Cache.
-    final List<PHLight> lights = bridge.getResourceCache().getAllLights();
-
     this.currentIndex = 0;
-    this.timer = new Timer(5000, new ActionListener() {
+    this.timer = new Timer(10000, new ActionListener() {
 
       @Override
       public void actionPerformed(ActionEvent e) {
+        phHueSDK.getSelectedBridge().setLightStateForDefaultGroup(lightStates.get(currentIndex));
         currentIndex = (currentIndex + 1) % lightStates.size();
-        for (PHLight light : lights) {
-          // Null is used here because we don't care about bridge response.
-          final PHLightListener nullListener = null;
-          phHueSDK.getSelectedBridge().updateLightState(light.getIdentifier(),
-              lightStates.get(currentIndex), nullListener);
-        }
         timer.restart();
       }
     });
   }
-  
+
   public void stopTimer() {
     this.timer.stop();
   }
